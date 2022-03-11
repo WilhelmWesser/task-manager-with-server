ae63042ea9ca30f799e2c0a49bb9a21b3016c59e
wilhelm wesser <wilhelmwesser228@gmail.com>
2022-03-11 23:22:11 +0200

    Creating backend part 2 is done

--- server/models/Task.js
+++ server/models/Task.js
@@ -4,7 +4,7 @@ const schema = new Schema({
     content: {type: String, required: true},
     heading: {type: String, required: true},
     pageId: {type: Schema.Types.ObjectId, ref: 'User', required: true},
-    priority: {type: Schema.Types.ObjectId, ref: 'Priority', required: true},
+    priority: {type: Object, required: true},
     responsible: {type: String, required: true},
     status: {type: String, required: true},
     terms: {type: String, required: true}
